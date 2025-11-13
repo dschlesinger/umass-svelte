675c1e4266cffc13554f5ee102b4a1417f6b7503
dschlesinger <denalischlesinger06@gmail.com>
2025-11-12 19:06:47 -0500

    Improve Navbar

--- src/lib/supabase/user.svelte.ts
+++ src/lib/supabase/user.svelte.ts
@@ -12,6 +12,19 @@ supabase.auth.onAuthStateChange(async (event, session) => {
     }
 })
 
+export async function logout() {
+    
+    const { error } = await supabase.auth.signOut()
+
+    if (error) {
+        console.error('Error signing out:', error.message)
+    } else {
+        console.log('User signed out successfully.')
+        // Redirect or update UI as needed
+    }
+
+}
+
 export async function login() {
     const { data, error } = await supabase.auth.signInWithOAuth({
         provider: 'google',
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,12 +4,23 @@
 
 	import Button from '$lib/components/ui/button/button.svelte';
 
-	import { login } from '$lib/supabase/user.svelte';
+	import { login, logout, user } from '$lib/supabase/user.svelte';
 
 	import { SvelteToast } from '@zerodevx/svelte-toast'
 
 	// Optionally set default options here
 	const options = {}
+
+	const routes = [
+		{
+			'name': 'Home',
+			'route': '/'
+		},
+		{
+			'name': 'Create Game',
+			'route': '/create-game'
+		}
+	]
 	
 	let { children } = $props();
 </script>
@@ -22,9 +33,47 @@
 
 <div class="flex flex-col h-screen w-screen">
 	<nav class="min-h-16 w-full flex bg-slate-950 items-center shrink-0">
-		<Button onclick={login} class="ml-auto mr-10" variant="outline">
-			Login
-		</Button>
+
+		<div class='w-32'>
+
+			<img 
+				class='w-12 aspect-square ml-10 rounded-full'
+				src={ user.current ? user.current.user_metadata.avatar_url : favicon } 
+				alt=""
+			>
+
+		</div>
+
+		<div class='grow flex justify-center gap-x-2'>
+
+			{#each routes as r}
+				<Button
+					class='min-w-24'
+					href={ r.route }
+				>
+					{ r.name }
+				</Button>
+			{/each}
+
+		</div>
+
+		<div class='w-32'>
+
+			{#if !user.current}
+	
+				<Button onclick={login} class="" variant="outline">
+					Login
+				</Button>
+
+			{:else}
+
+				<Button onclick={logout} class="" variant="destructive">
+					Logout
+				</Button>
+
+			{/if}
+
+		</div>
 	</nav>
 
 	<!-- Let this fill the rest of the screen and handle its own scroll -->
